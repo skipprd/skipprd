de4c9c29150863a04685bc87dc8c64802f591ef5
Paul Hudson <paul@drupaul.co.uk>
2021-05-02 13:42:23 +0100

    add random str function

--- src/Skipprd/Helpers.php
+++ src/Skipprd/Helpers.php
@@ -77,6 +77,19 @@ class Helpers
 
         return implode($pass); //turn the array into a string
     }
+
+    static public function randomStr($length = 8)
+    {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyz';
+        $pass = []; //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < $length; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+
+        return implode($pass); //turn the array into a string
+    }
 }
 
 
