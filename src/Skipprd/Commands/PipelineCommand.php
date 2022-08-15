ed327a6ee556d0e8cebf50bd6f7c17c9356e9add
Paul Hudson <paul@drupaul.co.uk>
2022-08-15 09:19:22 +0100

    ensure status update is regularly called on output plugin

--- src/Skipprd/Commands/PipelineCommand.php
+++ src/Skipprd/Commands/PipelineCommand.php
@@ -427,6 +427,8 @@ class PipelineCommand
                         $this->sock = $this->streamListen();
 
                         while (true) {
+
+                            // @todo - schema updates still need to be recieved
 //                            $this->streamRead(
 //                                [$this, 'serialiseOutput'],
 //                                [$this->outputPlugin, 'sync']
@@ -435,6 +437,7 @@ class PipelineCommand
                             $this->processInputBuffers();
                             $this->outputPlugin->buffer->flushFinalised();
                             $this->outputPlugin->sync();
+                            $this->scheduledStatusUpdate();
                             sleep(10);
                         }
 
