e7b538bc09e29a0ff341c57f4f8de7d3e6f90e07
Paul Hudson <paul@drupaul.co.uk>
2023-06-03 11:32:10 +0100

    clean s3 prefix

--- src/plugins/s3_input.rs
+++ src/plugins/s3_input.rs
@@ -109,6 +109,8 @@ impl DataSourceS3Plugin {
         let mut i = 0;
         let mut chunk_size_current = 0;
 
+        let mut inventory_prefix = inventory_prefix.trim_start_matches('/').to_string();
+
         if inventory_prefix == "/".to_string() || inventory_prefix == "./".to_string() {
             inventory_prefix = "".to_string();
         }
