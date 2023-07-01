00408a56c018c7a5536ada8e625fbac91f581ef4
Paul Hudson <paul@drupaul.co.uk>
2023-07-01 11:23:33 +0100

    test memory buffer

--- src/ingest_work.rs
+++ src/ingest_work.rs
@@ -18,6 +18,52 @@ use crate::{RUNNING};
 use lru::LruCache;
 use std::num::NonZeroUsize;
 
+pub struct Buffer {
+    data: Vec<u8>,
+    bytes: u64,
+}
+
+impl Buffer {
+    pub fn new() -> Self {
+        Buffer {
+            data: Vec::new(),
+            bytes: 0,
+        }
+    }
+
+    pub fn write(&mut self, data: &[u8]) {
+        self.data.extend_from_slice(data);
+        self.bytes += data.len() as u64;
+    }
+}
+
+pub struct Buffers {
+    pub(crate) buffers: HashMap<String, Buffer>,
+}
+
+impl Buffers {
+    pub fn new() -> Self {
+        Buffers {
+            buffers: HashMap::new(),
+        }
+    }
+
+    pub fn write(&mut self, key: &str, data: &[u8]) {
+        let buffer = self.buffers.entry(key.to_string()).or_insert(Buffer::new());
+        buffer.write(data);
+    }
+
+    pub fn clear(&mut self, key: &str) {
+        if let Some(buffer) = self.buffers.get_mut(key) {
+            buffer.data.clear();
+            buffer.bytes = 0;
+        }
+    }
+
+    pub fn clear_all(&mut self) {
+        self.buffers = HashMap::new();
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct IngestBatch {
@@ -55,29 +101,37 @@ impl Ingest {
         let data_dir = Config::get_data_dir();
         let output_dir = format!("{}/output", data_dir);
 
+        let mut rotated_files: Vec<String> = Vec::new();
+
         for (filename, output_file) in output_files.iter_mut() {
             output_file.file.flush().expect(&format!("Could not flush file {}", filename));
 
             if force || Ingest::is_file_size_exceeded(&output_file) || Ingest::is_file_time_exceeded(&output_file) {
                 if output_file.bytes > 0 { // don't flush empty files when forced
-                    output_file.bytes = 0;
-                    output_file.upated_at = UNIX_EPOCH;
-                    output_file.rotated = Some(true);
+                    // output_file.bytes = 0;
+                    // output_file.upated_at = UNIX_EPOCH;
+                    // output_file.rotated = Some(true);
 
                     let new_filename = format!(
                         "{}/done/{}-{}",
                         output_dir,
                         Helpers::random_str(12),
-                        filename
+                        &filename
                     );
-                    let old_path = format!("{}/{}", output_dir, filename);
+                    let old_path = format!("{}/{}", output_dir, &filename);
 
                     match fs::rename(&old_path, &new_filename) {
                         Ok(_) => {},
                         Err(_) => {}
                     };
+
+                    rotated_files.push(filename.to_string());
                 }
             }
+        }
+
+        for filename in rotated_files {
+            output_files.pop(&filename);
         }
 
         if force {
@@ -140,6 +194,8 @@ impl Ingest {
         let metrcis_clone = metrics.clone();
         let offset_db_clone = offset_db.clone();
 
+        let mut buffers: Buffers = Buffers::new();
+
         let mut output_files = match OUTPUT_FILES_STATIC.lock() {
             Ok(output_files) => output_files,
             Err(err) => {
@@ -154,8 +210,6 @@ impl Ingest {
         for ingest_batch in datas {
             let mut buf_str: String = String::new();
 
-            // have offsets, don't bother checking each line offset if not.
-            // relevant when processing a new file, which is most of the time
             let has_offsets =
                 offset_db_clone.validate(&ingest_batch.offset_key, OffsetTypes::Closed, 0);
 
@@ -205,50 +259,7 @@ impl Ingest {
                             Some(&skpr_partition),
                             skpr_time_bucket,
                         );
-                        let output_file = format!("{}/{}", output_dir.clone(), &output_file_name);
 
-                        if output_files.peek(&output_file_name).is_none() {
-                            let f = OpenOptions::new()
-                                .create(true)
-                                .write(true)
-                                .append(true)
-                                .open(output_file.clone())
-                                .unwrap();
-
-                            let mut writer = BufWriter::with_capacity(WRITE_BUF_SIZE, f);
-
-                            let new_file = match std::fs::metadata(&output_file) {
-                                Ok(metadata) => {
-
-                                    let secs_since_epoch = metadata.modified().unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs();
-                                    let time = UNIX_EPOCH + Duration::from_secs(secs_since_epoch);
-
-                                    OutputFile {
-                                        bytes: metadata.len(),
-                                        upated_at: time,
-                                        file: writer,
-                                        rotated: None
-                                    }
-                                },
-                                Err(err) => {
-                                    OutputFile {
-                                        bytes: record_bytes,
-                                        upated_at: aprox_now,
-                                        file: writer,
-                                        rotated: None
-                                    }
-                                }
-                            };
-
-                            // If the cache is full, remove and flush the least recently used item.
-                            if output_files.len() == output_files.cap().get() {
-                                if let Some((filename, mut evicted)) = output_files.pop_lru() {
-                                    evicted.file.flush().expect(&format!("Could not flush file {}", filename));
-                                }
-                            }
-
-                            output_files.put(output_file_name.clone(), new_file);
-                        }
 
                         let mut meta = metadata_clone.lock().unwrap();
                         if meta.get(&skpr_namespace).is_none() {
@@ -262,18 +273,11 @@ impl Ingest {
                             flatten
                         );
 
-                        buf_str = msg.to_string() + "\n";
-
-                        {
-                            let mut output_file = output_files
-                                .get_mut(&output_file_name)
-                                .unwrap();
-                            output_file.file.write_all(buf_str.as_bytes()).unwrap();
-                            output_file.bytes += record_bytes;
-                            output_file.upated_at = aprox_now;
-                        }
-
-                        buf_str.clear();
+                        let buf_str = msg.to_string() + "\n";
+                        buffers.write(&output_file_name, buf_str.as_bytes());
+                            // .or_insert_with(Buffer::new);
+                        // buffer.write(buf_str.as_bytes());
+                        // buffer.bytes += record_bytes;
 
                         i += 1;
                         j += 1;
@@ -287,9 +291,67 @@ impl Ingest {
 
         }
 
+        // Flush all buffers to their respective files.
+
+        for (filename, buffer) in buffers.buffers.iter() {
+
+            let output_file = format!("{}/{}", output_dir.clone(), &filename);
+
+            if output_files.peek(filename).is_none() {
+                let f = OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .append(true)
+                    .open(output_file.clone())
+                    .unwrap();
+
+                let mut writer = BufWriter::with_capacity(WRITE_BUF_SIZE, f);
+
+                let new_file = match std::fs::metadata(&output_file) {
+                    Ok(metadata) => {
+
+                        let secs_since_epoch = metadata.modified().unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs();
+                        let time = UNIX_EPOCH + Duration::from_secs(secs_since_epoch);
+
+                        OutputFile {
+                            bytes: metadata.len(),
+                            upated_at: time,
+                            file: writer,
+                            rotated: None
+                        }
+                    },
+                    Err(err) => {
+                        OutputFile {
+                            bytes: 0,
+                            upated_at: aprox_now,
+                            file: writer,
+                            rotated: None
+                        }
+                    }
+                };
+
+                // If the cache is full, remove and flush the least recently used item.
+                if output_files.len() == output_files.cap().get() {
+                    if let Some((filename, mut evicted)) = output_files.pop_lru() {
+                        evicted.file.flush().expect(&format!("Could not flush file {}", filename));
+                    }
+                }
+
+                output_files.put(filename.clone(), new_file);
+            }
+
+            if let Some(mut output_file) = output_files.get_mut(filename) {
+                output_file.file.write_all(&buffer.data).unwrap();
+                output_file.bytes += buffer.bytes;
+                output_file.upated_at = aprox_now;
+            }
+        }
+
         Self::flush_buffers(false, &mut output_files);
 
         offset_db_clone.flush();
+
+        buffers.clear_all();
 
         let mut counter_lock = metrcis_clone.lock().unwrap();
         counter_lock.ingeted_current += j;
