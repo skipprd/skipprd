59356437264d3608bae9847187954694975f22d3
Paul Hudson <paul@drupaul.co.uk>
2023-08-21 15:41:34 +0100

    switch to enable RwLock profiling, to avoid any overhead when in production

--- src/helpers/timed_rwlock.rs
+++ src/helpers/timed_rwlock.rs
@@ -4,6 +4,7 @@ use std::collections::{HashMap};
 use lazy_static::lazy_static;
 
 lazy_static! {
+    static ref PROFILE_PERFORMANCE: bool = false;
     // This will hold the lock names that are currently being waited on and the time they started waiting.
     static ref WAITING_ON: Mutex<HashMap<String, Instant>> = Mutex::new(HashMap::new());
     // This will hold the total wait times for each lock over the whole execution.
@@ -26,19 +27,23 @@ impl<T> TimedRwLock<T> {
     }
 
     pub fn read(&self) -> Result<RwLockReadGuard<'_, T>, PoisonError<RwLockReadGuard<'_, T>>> {
-        {
-            let mut waiting_on = WAITING_ON.lock().unwrap();
-            waiting_on.insert(self.name.clone(), Instant::now());
+        if *PROFILE_PERFORMANCE {
+            {
+                let mut waiting_on = WAITING_ON.lock().unwrap();
+                waiting_on.insert(self.name.clone(), Instant::now());
+            }
         }
 
         let result = self.lock.read();
 
-        {
-            let mut waiting_on = WAITING_ON.lock().unwrap();
-            if let Some(start_time) = waiting_on.remove(&self.name) {
-                let elapsed = start_time.elapsed();
-                let mut total_wait_time = TOTAL_WAIT_TIMES.lock().unwrap();
-                *total_wait_time.entry(self.name.clone()).or_insert(Duration::new(0, 0)) += elapsed;
+        if *PROFILE_PERFORMANCE {
+            {
+                let mut waiting_on = WAITING_ON.lock().unwrap();
+                if let Some(start_time) = waiting_on.remove(&self.name) {
+                    let elapsed = start_time.elapsed();
+                    let mut total_wait_time = TOTAL_WAIT_TIMES.lock().unwrap();
+                    *total_wait_time.entry(self.name.clone()).or_insert(Duration::new(0, 0)) += elapsed;
+                }
             }
         }
 
@@ -46,19 +51,23 @@ impl<T> TimedRwLock<T> {
     }
 
     pub fn write(&self) -> Result<RwLockWriteGuard<'_, T>, PoisonError<RwLockWriteGuard<'_, T>>> {
-        {
-            let mut waiting_on = WAITING_ON.lock().unwrap();
-            waiting_on.insert(self.name.clone(), Instant::now());
+        if *PROFILE_PERFORMANCE {
+            {
+                let mut waiting_on = WAITING_ON.lock().unwrap();
+                waiting_on.insert(self.name.clone(), Instant::now());
+            }
         }
 
         let result = self.lock.write();
 
-        {
-            let mut waiting_on = WAITING_ON.lock().unwrap();
-            if let Some(start_time) = waiting_on.remove(&self.name) {
-                let elapsed = start_time.elapsed();
-                let mut total_wait_time = TOTAL_WAIT_TIMES.lock().unwrap();
-                *total_wait_time.entry(self.name.clone()).or_insert(Duration::new(0, 0)) += elapsed;
+        if *PROFILE_PERFORMANCE {
+            {
+                let mut waiting_on = WAITING_ON.lock().unwrap();
+                if let Some(start_time) = waiting_on.remove(&self.name) {
+                    let elapsed = start_time.elapsed();
+                    let mut total_wait_time = TOTAL_WAIT_TIMES.lock().unwrap();
+                    *total_wait_time.entry(self.name.clone()).or_insert(Duration::new(0, 0)) += elapsed;
+                }
             }
         }
 
