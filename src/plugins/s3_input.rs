151f03a2a8727788dbd60c4d9d3c5c22fa5d6da6
Paul Hudson <paul@drupaul.co.uk>
2023-11-23 11:35:08 +0000

    logs

--- src/plugins/s3_input.rs
+++ src/plugins/s3_input.rs
@@ -229,7 +229,9 @@ impl DataSourceS3Plugin {
                         }
 
                         if skipped_objects > 0 {
-                            // println!("Skipped {} objects... already processed", skipped_objects);
+                            if skipped_objects >= 100000 {
+                                println!("Skipped {} objects... already processed", skipped_objects);
+                            }
                             skipped_objects = 0;
                         }
                     }
