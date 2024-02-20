69f028f61056ea1d1e07bbf3d84c80bdb46b1eac
Paul Hudson <paul@drupaul.co.uk>
2024-02-20 15:56:47 +0000

    more atomic offset commit

--- src/buffer/ingest_buffer.rs
+++ src/buffer/ingest_buffer.rs
@@ -170,7 +170,6 @@ impl Buffers {
 
             record_batches.push(decoder.flush().unwrap().unwrap());
 
-
             // let mut ingest_batch = WalRecordBatches::new(offset, record_batches);
 
             wal_file.write_to_stream(&record_batches)?;
@@ -179,17 +178,17 @@ impl Buffers {
             // wal_file_partition.bytes += wal_file.bytes;
             // println!("Wrote records to WAL file: {}", wal_file.path.to_str().unwrap());
 
-            wal_file.flush()?;
-
             wal_file.close()?;
 
             let offset_key = OffsetKey {
-                namespace: wal_file.offset.source_namespace.clone(),
-                partition: wal_file.offset.source_partition.clone(),
+                namespace: ingest_buffer_batch.offset.source_namespace.clone(),
+                partition: ingest_buffer_batch.offset.source_partition.clone(),
             };
-
-            offsets_db.insert(&offset_key, OffsetTypes::Line, wal_file.offset.position.clone());
+            offsets_db.insert(&offset_key, OffsetTypes::Line, ingest_buffer_batch.offset.position.clone());
             offsets_db.insert(&offset_key, OffsetTypes::Closed, 1);
+
+            wal_file.flush()?;
+
 
             partition_entry.push(wal_file);
 
