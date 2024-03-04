e99ad214c4e96dad3d9fb260a3b3c31f2a68e288
Paul Hudson <paul@drupaul.co.uk>
2024-03-04 10:04:36 +0000

    typo

--- src/plugins/s3_input.rs
+++ src/plugins/s3_input.rs
@@ -228,12 +228,12 @@ impl DataSourceS3Plugin {
                                 if chunk_size_current >= chunk_size {
                                     // println!("Proccessing {} Objects, totalling {} bytes (batch size config {} bytes)", i, chunk_size_current, chunk_size);
 
-                                    // self.download_and_ingest(
-                                    //     &s3_bucket,
-                                    //     &outputs,
-                                    //     &offsets_clone
-                                    // )
-                                    // .await;
+                                    self.download_and_ingest(
+                                        &s3_bucket,
+                                        &outputs,
+                                        &offsets_clone
+                                    )
+                                    .await;
 
                                     outputs = Vec::new();
                                     i = 0;
@@ -265,12 +265,12 @@ impl DataSourceS3Plugin {
                         println!("Reached end of S3 pagination");
 
                         if !outputs.is_empty() {
-                            // self.download_and_ingest(
-                            //     &s3_bucket,
-                            //     &outputs,
-                            //     &offsets_clone,
-                            // )
-                            //     .await;
+                            self.download_and_ingest(
+                                &s3_bucket,
+                                &outputs,
+                                &offsets_clone,
+                            )
+                                .await;
                         }
 
                         break;
