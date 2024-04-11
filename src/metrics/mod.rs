b77ccc082e4c3b949d7b14a3a2a20943b3dc5dca
Paul Hudson <paul@drupaul.co.uk>
2024-04-11 11:08:20 +0100

    fix

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -350,6 +350,7 @@ impl Metrics {
                 "wal_index_files_total": metrics.wal_index_files_total,
                 "wal_index_bytes_total": metrics.wal_index_bytes_total,
                 "wal_write_bytes_total": metrics.wal_write_bytes_total,
+                "wal_index_metrics": metrics.wal_index_metrics,
                 "bytes_total": metrics.source_bytes_total,
                 "wal_write_bytes_total": metrics.wal_write_bytes_total,
                 "wal_write_bytes_current": wal_write_bytes_current,
@@ -367,8 +368,7 @@ impl Metrics {
                 "parquet_persisted_objects_current": parquet_persisted_objects_current,
                 "lock_wait_times": wait_times,
             },
-            // have to be careful not to change 'metrics' from map to record
-            "wal_index_metrics": metrics.wal_index_metrics,
+
             "type": "metric",
             "run_id": metrics.run_id,
             "tenant_id": tenant_id,
